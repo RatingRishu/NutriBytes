9008bd6bd96a9e832ea88ac525d517a5278eab00
RatingRishu <rairishabh5001@gmail.com>
2024-05-27 19:27:50 +0530

    Recipe data show

--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NutriServiceService } from '../nutri-service.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
+  ingredientFoodData : any = 'paneer'
+  ingredientData : any = ['200 gram paneer','1 cup rice']
 
+  foodData : any;
+  foodItemData : any;
+
+  query: string = ''; 
+
+
+
+  constructor(private nutriService: NutriServiceService) {
+
+    // this.nutriService.getNutriData(this.ingredientData).subscribe(data => {
+    //   console.log(data);
+    // });
+
+    // this.nutriService.getFoodData(this.ingredientFoodData).subscribe(data => {
+    //   this.foodData = data;
+    //   console.log(this.foodData);
+    //   this.foodItemData = this.foodData.parsed[0];
+    //   console.log(this.foodItemData.label);
+    // });
+  }
+
+  fetchFoodData(): void {
+    if (this.query.trim()) { // Ensure the query is not empty
+      this.nutriService.getFoodData(this.query).subscribe(data => {
+        this.foodData = data;
+      });
+    }
+  }
 }
