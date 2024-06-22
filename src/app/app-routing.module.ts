ff5db16bc6f14099cb6f9db4d279c9fb98c82221
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2024-06-22 20:47:24 +0530

    aboutus nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { HomeComponent } from './home/home.component';
 import { BlogsComponent } from './blogs/blogs.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'list', component: RecipesListComponent },
   { path: 'blogs', component: BlogsComponent },
+  { path: 'about', component: AboutusComponent },
 ];
 
 @NgModule({
